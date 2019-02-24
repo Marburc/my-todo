c0b7f892c977baf101b5109a325020457a1fc690
Martin <martin.burchardt@yahoo.de>
2019-02-24 18:46:45 +0100

    added firebase and testarea in login

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Das ist der LogIn Bereich</h1>
+  <div>
+    <h1>Das ist der LogIn Bereich</h1>
+    <h2>Vorrübergehender Firestore Testbereich</h2>
+  </div>
 </template>
+<script>
+import db from "./firebaseInit.js";
+export default {
+  created() {
+    db.collection("todos")
+      .get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+          console.log(doc.data());
+        });
+      });
+  }
+};
+</script>
+
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+
+    apiKey: "AIzaSyAGijOpsxzlZMf8_gqUd87o7SSNwKriNMY",
+    authDomain: "testprojekt-14901.firebaseapp.com",
+    databaseURL: "https://testprojekt-14901.firebaseio.com",
+    projectId: "testprojekt-14901",
+    storageBucket: "testprojekt-14901.appspot.com",
+    messagingSenderId: "711003147450"
+}
